b0acf69bfcf131e4c79c830c55498f6ac9c9b010
sashashin <shina3@rpi.edu>
2017-02-23 16:02:05 -0500

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,6 +1,6 @@
 var appleSauceTemplate = _.template(`
 	<div class='index-centered-div'>
-		<h1>For-Team-Applesauce</h1>
+		<h1>Your-Odyssey</h1>
 		<p>Nothing suspicious, simply delicious.</p>
 		<img src="images/applesauce.jpg" alt="applesauce-photo" class="applesauce-photo">
 	</div>
