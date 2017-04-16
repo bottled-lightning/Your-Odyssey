0a7ddc7b147461e50be67634ed5f1d30c3cbf4f1
leah <lmagrane28@gmail.com>
2017-04-15 21:10:46 -0400

    added in API support, put in rough parsing

--- views.js
+++ views.js
@@ -46,29 +46,84 @@ var flightSearchView = Backbone.View.extend({
 	}
 });
 
-// function callback(response_data){
-// 	console.log("helo");
-// 	console.log(response_data);
-// 	flights = response_data;
-// };
+function fback(response_data){
+ 	console.log("helo");
+ 	console.log(response_data);
+ 	flights = response_data;
+};
 
 // This is code that makes the actual AJAX request
 function makeRequest(OUTBOUND_LOCATION, INBOUND_LOCATION, OUTBOUND_DATE, INBOUND_DATE){
+	var data = {};
 	$.ajax({	
 		//url: 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/'+ OUTBOUND_LOCATION + '/' + INBOUND_LOCATION + '/' + OUTBOUND_DATE + '/' + INBOUND_DATE + '?apiKey=' + 're686126617468938158317525284308',
-		url: 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/LAX/JFK/2017-04-12/2017-04-13?apiKey=re686126617468938158317525284308',
-        //crossDomain: true,
-		type: 'GET',
-       	dataType:'jsonp',
-       	jsonpCallback: 'callback',
-       	success: function(response) {
-        	console.log('callback success');
-    	},
-		error: function(XMLHttpRequest, textStatus, errorThrown){
-			alert("Error using travel API. textStatus: " + textStatus + errorThrown);
+		url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDk1wWH9nVv_QzJJmc-aHr7eaUpslumw1U',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify(FlightRequest),
+		success: function(data){
+			var trips = data["trips"];
+			console.log(trips);
+			for (i = 0; i < trips["tripOption"].length; i++) {
+				var airlineStr = "";
+				trips["tripOption"][i]["slice"]["0"]["segment"].forEach(function(e){
+						airlineStr += e["leg"]["0"].destination + "\n";
+				});
+				var timeStr = "";
+				trips["tripOption"][i]["slice"]["0"]["segment"].forEach(function(e){
+						timeStr += e["leg"]["0"].departureTime + "\n";
+				});
+   			$('.flight-bin').append(flightCardTemplate(
+				{
+					departingLoc: OUTBOUND_LOCATION,
+					arrivalLoc: INBOUND_LOCATION,
+					departingTime: timeStr,
+					returnTime: 'need to add',
+					// NOTE: WE NEED TO THEN TRANSLATE A CARRIER ID -> CARRIER NAME
+					airline: airlineStr,
+					cost: trips["tripOption"][i].saleTotal,
+					adults: '1',
+					children: '1'
+				}
+			));
 		}
+			
+		},
+		error: function(data) {
+			console.log(data);
+		}
+		
 	});
+	/*.done(function(data){
+		console.log("??");
+		for (i = 0; i < data["Quotes"].length; i++) { 
+   			$('.flight-bin').append(flightCardTemplate(
+				{
+					departingLoc: OUTBOUND_LOCATION,
+					arrivalLoc: INBOUND_LOCATION,
+					departingTime: data["Quotes"][i].QuoteDateTime,
+					returnTime: 'need to add',
+					// NOTE: WE NEED TO THEN TRANSLATE A CARRIER ID -> CARRIER NAME
+					airline: data["Quotes"][i].CarrierIds,
+					cost: data["Quotes"][i].MinPrice,
+					adults: '1',
+					children: '1'
+				}
+			));
+		}	
+	});*/
 };
+
+ function localJsonpCallback(json) {
+        if (!json.Error) {
+            $('#resultForm').submit();
+        }
+        else {
+            $('#loading').hide();
+            $('#userForm').show();
+            alert(json.Message);
+        }
+    }
 
 // The javascript code for Flight-Selector.hmtl and everything it displays
 var flightSelectorView = Backbone.View.extend({
@@ -118,24 +173,9 @@ var flightSelectorView = Backbone.View.extend({
 		// console.log(descriptor.departing.split('T')[0]);
 		// console.log(descriptor.returning.split('T')[0]);
 		makeRequest(descriptor.from, descriptor.to, descriptor.departing.split('T')[0], descriptor.returning.split('T')[0]);
-
 		// This should be the code where we iteratively create
 		// flight cards using the JSON stored as "flights"
-		for (i = 0; i < flights["Quotes"].length; i++) { 
-   			$('.flight-bin').append(flightCardTemplate(
-				{
-					departingLoc: descriptor.from,
-					arrivalLoc: descriptor.to,
-					departingTime: flights["Quotes"][i].QuoteDateTime,
-					returnTime: 'need to add',
-					// NOTE: WE NEED TO THEN TRANSLATE A CARRIER ID -> CARRIER NAME
-					airline: flights["Quotes"][i].CarrierIds,
-					cost: flights["Quotes"][i].MinPrice,
-					adults: descriptor.adults,
-					children: descriptor.children
-				}
-			));
-		}	
+		
 
 		// Append a bunch of dummy data that is exactly the same
 		/*	
@@ -156,3 +196,25 @@ var flightSelectorView = Backbone.View.extend({
 		// fin
 	}
 });
+
+var FlightRequest = 
+{
+  "request": {
+    "slice": [
+      {
+        "origin": "LAX",
+        "destination": "JFK",
+        "date": "2017-04-15"
+      }
+    ],
+    "passengers": {
+      "adultCount": 1,
+      "infantInLapCount": 0,
+      "infantInSeatCount": 0,
+      "childCount": 0,
+      "seniorCount": 0
+    },
+    "solutions": 20,
+    "refundable": false
+  }
+}
