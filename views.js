d189e1770c8dda3665181f4095e269ab15f317d7
Alex Shin <shina3@rpi.edu>
2017-04-19 20:23:37 -0400

    not sure

--- views.js
+++ views.js
@@ -76,7 +76,7 @@ function makeRequest(OUTBOUND_LOCATION, INBOUND_LOCATION, OUTBOUND_DATE, INBOUND
 	var data = {};
 	$.ajax({	
 		//PLEASE DO NOT PUSH API KEYS
-		url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBZf3vCUJ_eeaaidE9pQ10n4BM-HoNBoCM',
+		url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=',
 		type: 'POST',
 		contentType: 'application/json',
 		data: JSON.stringify(FlightRequest),
